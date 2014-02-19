cbdec69ff4fb382cd5dfa7cd7bfa8bd42d4a2a2b
Daniel Stiner <danstiner@gmail.com>
2014-02-19 00:39:35 -0800

    Monadic state-driven Mem store functions

--- Store/Mem.hs
+++ Store/Mem.hs
@@ -3,24 +3,47 @@ module Store.Mem (
     createStore
   , get
   , put
-  , Store.Mem.delete
+  , delete
   , MemStore
 ) where
 
 import qualified Blob
-import Data.Map as Map
+import qualified Data.Map as Map
+import qualified Control.Monad.Trans.State as State
+import Control.Monad.Trans.Class (lift)
 
-newtype MemStore = MemStore (Map Blob.Id Blob.Data) deriving (Show)
+newtype MemStore = MemStore (Map.Map Blob.Id Blob.Data) deriving (Show)
+
+type MemStateT = State.StateT MemStore
+type MemState = State.State MemStore
 
 createStore :: MemStore
-createStore = MemStore empty
+createStore = MemStore Map.empty
+
+getS :: Blob.Id -> MemStateT Maybe Blob.Data
+getS i = do
+	(MemStore m) <- State.get
+	v <- lift $ Map.lookup i m
+	return v
+
+putS :: Blob.Blob -> MemState ()
+putS (Blob.Blob i dat) = do
+	(MemStore m) <- State.get
+	let newStore = MemStore $ Map.insert i dat m
+	State.put $ newStore
+
+deleteS :: Blob.Id -> MemState ()
+deleteS i = do
+	(MemStore m) <- State.get
+	let newStore = MemStore $ Map.delete i m
+	State.put $ newStore
 
 get :: Blob.Id -> MemStore -> Maybe Blob.Data
 get i (MemStore m) = Map.lookup i m
 
 put :: Blob.Blob -> MemStore -> MemStore
 put (Blob.Blob i dat) (MemStore m) =
-  MemStore $ insert i dat m
+  MemStore $ Map.insert i dat m
 
 delete :: Blob.Id -> MemStore -> MemStore
 delete i (MemStore m) =
