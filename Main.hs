51a10c645a9fc9e109c88f7f936a5a0449c0ec6c
Daniel Stiner <danstiner@gmail.com>
2014-03-12 22:49:37 -0700

    Removed extra print and commented lines from Main.hs

--- Main.hs
+++ Main.hs
@@ -8,11 +8,7 @@ import System.Directory as Dir
 
 main :: IO ()
 main = do
-  print "Hello Cloud World"
   [path] <- getArgs
---  fileStore <- traverseAndStore path InMem.createStore
---  print fileStore
   home <- Dir.getHomeDirectory
---  traverseAndStoreFlat path $ Flat.createStore (home ++ "/.bitcloud")
   Plumbing.writeTree path $ Mem.createStore
   Plumbing.writeTree path $ Flat.createStore (home ++ "/.bitcloud")
