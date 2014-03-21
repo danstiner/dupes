e6cc818171e92324e555a9e79c50211aad593fc1
Daniel Stiner <danstiner@gmail.com>
2014-03-21 00:07:00 -0700

    Introduced command-line option parsing

--- Main.hs
+++ Main.hs
@@ -13,20 +13,49 @@ import System.Directory as Dir
 import System.Log.Logger
 import System.FilePath ( (</>) )
 import System.Directory as Directory
+import Options.Applicative
+
+data CommandOptions = CommandOptions
+  { verbose :: Bool
+  , quiet :: Bool
+  , path :: String }
+
+sample :: Parser CommandOptions
+sample = CommandOptions
+  <$> switch
+      ( long "verbose"
+     <> help "Whether to be loud" )
+  <*> switch
+      ( long "quiet"
+     <> help "Whether to be quiet" )
+  <*> argument str (metavar "PATH")
+
 
 main :: IO ()
-main = do
+main = execParser opts >>= runWithOptions
+	where
+		parser = (helper <*> sample)
+		desc = ( fullDesc
+			<> progDesc "Print a greeting for TARGET"
+			<> header "hello - a test for optparse-applicative" )
+		opts = info parser desc
+
+runWithOptions :: CommandOptions -> IO ()
+runWithOptions options = do
   home <- Dir.getHomeDirectory
   let appUserDir = home </> ".clod"
   Directory.createDirectoryIfMissing False appUserDir
 
+  let logLevel = case ((quiet options), (verbose options)) of
+  	(True, False) -> ERROR
+  	(False, True) -> DEBUG
+  	otherwise -> WARNING
 
-  Logging.registerLogger appUserDir WARNING
+  Logging.registerLogger appUserDir logLevel
   Telemetry.registerLogger appUserDir
 
   infoM App.logTag "Application launching"
 
-  [path] <- getArgs
-  treeId <- Plumbing.writeTree path $ Flat.createStore (appUserDir)
+  treeId <- Plumbing.writeTree (path options) $ Flat.createStore (appUserDir)
   Plumbing.setRef "master" treeId $ LevelDB.createStore (appUserDir </> "leveldb")
   return ()
