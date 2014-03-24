b591397f77a20a4dfc04c7f76985e66236f9f13f
Daniel Stiner <danstiner@gmail.com>
2014-03-23 19:57:05 -0700

    Updated logging, now we: * Always log with level NOTICE to our app logs * Log with configurable level to stderr

--- Logging.hs
+++ Logging.hs
@@ -2,14 +2,18 @@ module Logging (
 	registerLogger
 ) where
 
-import System.Log.Handler.Log4jXML as Log4j
-import System.Log.Logger
 import System.FilePath ( (</>) )
-
-import qualified App
+import System.IO
+import System.Log.Handler.Log4jXML as Log4j
+import System.Log.Handler.Simple as SimpleLog
+import System.Log.Logger
 
 registerLogger :: FilePath -> Priority -> IO ()
 registerLogger outfolder p = do
-  s <- Log4j.log4jFileHandler' (outfolder </> "log.xml") p
-  updateGlobalLogger rootLoggerName (setLevel p . setHandlers [s])
+  app <- Log4j.log4jFileHandler' (outfolder </> "log.xml") NOTICE
+  std <- stderrLogger p
+  updateGlobalLogger rootLoggerName (setLevel p . setHandlers [app, std])
 
+
+stderrLogger :: Priority -> IO (GenericHandler Handle)
+stderrLogger p = streamHandler stderr p
