989e57fc8afb8522da3a175ed0759b857155baad
Daniel Stiner <danstiner@gmail.com>
2014-04-08 01:38:46 -0700

    Formatting in Store/Blob.hs

--- Store/Blob.hs
+++ Store/Blob.hs
@@ -1,11 +1,11 @@
 module Store.Blob (
-	BlobStore (..)
+    BlobStore (..)
 ) where
 
 import qualified Blob
 import qualified Control.Monad.Trans.State as State
 
 class BlobStore s where
-	get :: Blob.Id -> (State.StateT s IO) (Maybe Blob.Blob)
-	put :: Blob.Blob -> State.StateT s IO ()
-	delete :: Blob.Id -> State.StateT s IO ()
+  get :: Blob.Id -> (State.StateT s IO) (Maybe Blob.Blob)
+  put :: Blob.Blob -> State.StateT s IO ()
+  delete :: Blob.Id -> State.StateT s IO ()
