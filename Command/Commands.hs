00fca9bc502c7a0f9f176e3cb25c2c15baa0163a
Daniel Stiner <danstiner@gmail.com>
2014-04-14 00:15:56 -0700

    ls-files command to list current contents of index

--- Command/Commands.hs
+++ Command/Commands.hs
@@ -5,6 +5,7 @@ module Command.Commands (
 ) where
 
 import qualified Command.HashObject as HashObject
+import qualified Command.LsFiles as LsFiles
 import qualified Command.UpdateIndex as UpdateIndex
 
 import Options.Applicative
@@ -12,12 +13,15 @@ import Options.Applicative
 data Command 
 	= HashObject HashObject.Options
 	| UpdateIndex UpdateIndex.Options
+	| LsFiles LsFiles.Options
 
 parser :: Parser Command
 parser = subparser (
      ( command "hash-object" (fmap HashObject HashObject.parserInfo) )
+  <> ( command "ls-files" (fmap LsFiles LsFiles.parserInfo) )
   <> ( command "update-index" (fmap UpdateIndex UpdateIndex.parserInfo) )
   )
 
 run :: Command -> IO ()
 run (HashObject opt) = HashObject.run opt
+run (LsFiles opt) = LsFiles.run opt
---
+++ Command/LsFiles.hs
@@ -0,0 +1,52 @@
+module Command.LsFiles (
+	Options
+  , parserInfo
+  , run
+) where
+
+import Data.Binary as Binary
+import Data.ByteString
+import Options.Applicative
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as L
+import qualified Database.LevelDB.Higher as Level
+import System.FilePath ( (</>) )
+
+import qualified Blob
+import qualified Settings
+
+data Options = Options
+  { cached :: Bool }
+
+parserInfo :: ParserInfo Options
+parserInfo = info parser
+  (progDesc "Show information about files in the index and the working tree")
+
+parser :: Parser Options
+parser = Options
+  <$> switch
+      ( long "cached"
+     <> short 'c'
+     <> help "Show cached files in the output (default)." )
+
+keySpace :: ByteString
+keySpace = C.pack "MyKeySpace"
+
+run :: Options -> IO ()
+run opt = do
+  appDir <- Settings.getAppDir
+  let path = appDir </> "leveldb"
+  
+  items <- Level.runLevelDB path Level.def (Level.def, Level.def) keySpace $ do
+    Level.scan (C.pack ("index/")) (Level.queryItems)
+
+  mapM (Prelude.putStrLn . showItem) items
+
+  return ()
+
+
+showItem :: Level.Item -> String
+showItem item =
+  (C.unpack $ fst item)
+  ++ " " ++
+  (Blob.toString $ Binary.decode $ L.fromStrict $ snd item)
