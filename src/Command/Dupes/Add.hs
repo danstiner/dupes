e50acf3041d39471fa2bb83c7d8436963c221bc7
Daniel Stiner <danstiner@gmail.com>
2014-08-18 22:44:19 -0700

    Dupes: Updated implementation for add that actually stores added paths

--- src/Command/Dupes/Add.hs
+++ src/Command/Dupes/Add.hs
@@ -7,14 +7,19 @@ module Command.Dupes.Add (
 ) where
 
 import Dupes
+import Settings
 
 import Control.Monad.Trans ( lift )
 import Data.Foldable ( Foldable, traverse_ )
 import Data.List ( delete, (\\), sort )
 import Data.Machine hiding ( run )
+import Data.Serialize (encode,decode)
 import Options.Applicative
-import System.FilePath ( (</>) )
+import qualified Data.ByteString.Char8 as C
+import qualified Database.LevelDB as LDB
+import qualified Database.LevelDB.Higher as Level
 import System.Directory
+import System.FilePath ( (</>) )
 
 data Options = Options
   { optStdin :: Bool
@@ -35,7 +40,7 @@ parser = Options
 run :: Options -> IO ()
 run opt = runT_ machine
   where
-    machine = pathSource opt ~> traversePath ~> toPathKeyP ~> printPathKey
+    machine = pathSource opt ~> processPath
 
 printPathKey :: ProcessT IO PathKey ()
 printPathKey = repeatedly $ await >>= \a -> lift (putStrLn $ show a)
@@ -54,25 +59,65 @@ pathSource opt =
       c <- getContents
       return (lines c)
 
+processPath :: ProcessT IO FilePath ()
+processPath = repeatedly $ do
+  path <- await
+  appDir <- lift $ Settings.getAppDir
+  items <- lift $ Level.runCreateLevelDB (appDir </> "leveldb") (C.pack "Dupes") $ Level.withSnapshot $ Level.scan (encode $ toPathKey path) Level.queryItems
+
+  let merged = (traversePath path ~> toPathKeyP) `mergeOrderedStreams` (source items ~> levelToPathKey) in
+    lift $ runT_ (merged ~> store)
+
+store :: ProcessT IO (MergedOperation PathKey) ()
+store = repeatedly $ do
+  key <- await
+
+  lift $ case key of
+    LeftOnly a -> putStr "Add " >> putStrLn (show a) >> put a
+    RightOnly a -> putStr "Del " >> putStrLn (show a) >> rm a
+    Both _ -> return ()
+
+  where
+    put key = do
+      appDir <- Settings.getAppDir
+      Level.runCreateLevelDB (appDir </> "leveldb") (C.pack "Dupes") $ Level.put (encode key) (encode key)
+    rm key = do
+      appDir <- Settings.getAppDir
+      Level.runCreateLevelDB (appDir </> "leveldb") (C.pack "Dupes") $ Level.delete (encode key)
+
+levelToPathKey :: ProcessT IO (Level.Key, Level.Value) PathKey
+levelToPathKey = repeatedly $ do
+  (key, _) <- await
+  case decode key of
+    Left _ -> return ()
+    Right k -> yield k
+
 toPathKeyP :: ProcessT IO FilePath PathKey
 toPathKeyP = repeatedly $ do
   path <- await
   yield $ toPathKey path
 
-traversePath :: ProcessT IO FilePath FilePath
-traversePath = repeatedly $ do
-  path <- await
-  traversePath' path
+traversePath :: FilePath -> SourceT IO FilePath
+traversePath = construct . plan
+  where
+  plan path = do
+    isDir <- lift $ doesDirectoryExist path
 
-traversePath' path = do
-  yield path
-  isDir <- lift $ doesDirectoryExist path
-  if isDir
-    then do
-      contents <- lift $ getDirectoryContents path
-      let actual = sort . map (path </>) $ contents \\ [".", ".."]
-      mapM_ traversePath' actual
-    else return ()
+    yield path
+
+    if isDir
+      then do
+        contents <- lift $ getDirectoryContents path
+        let actual = map (path </>) $ contents \\ [".", ".."]
+        slashed <- lift $ addSlashes actual
+        mapM_ plan $ sort slashed
+      else return ()
+  addSlashes = mapM addSlash
+  addSlash path = do
+    isDir <- doesDirectoryExist path
+    if isDir
+      then return (path ++ "/")
+      else return path
 
 sourceT :: Monad m => Foldable f => m (f b) -> SourceT m b
 sourceT mxs = construct $ do
