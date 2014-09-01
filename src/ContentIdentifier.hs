91040f54a4c5931abcf481ba07c357000d524f4a
Daniel Stiner <danstiner@gmail.com>
2014-09-01 04:17:54 -0700

    ContentIdentifier: Add support for SHA1 id's

--- src/ContentIdentifier.hs
+++ src/ContentIdentifier.hs
@@ -19,6 +19,7 @@ import Data.Serialize
 import Data.Digest.CRC32
 import GHC.Generics (Generic)
 import qualified Crypto.Hash.MD5 as MD5
+import qualified Crypto.Hash.SHA1 as SHA1
 import qualified Crypto.Hash.SHA3 as SHA3
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base16 as Base16
@@ -28,10 +29,10 @@ import qualified Text.Read as R
 import qualified Text.ParserCombinators.ReadP as ReadP
 import Data.Char (isHexDigit)
 
-data Algro = CRC32 | MD5 | SHA3_256 deriving (Generic)
+data Algro = CRC32 | MD5 | SHA1 | SHA3_256 deriving (Generic)
 type Digest = B.ByteString
 type HashSize = Int
-data Id = CRC32_Id Digest | MD5_Id Digest | SHA3 HashSize Digest | Nil deriving (Eq, Ord, Generic)
+data Id = CRC32_Id Digest | MD5_Id Digest | SHA1_Id Digest | SHA3 HashSize Digest | Nil deriving (Eq, Ord, Generic)
 
 type IdString = String
 type Type = Algro
@@ -51,11 +52,13 @@ nil = Nil
 
 hashLazy :: Algro -> L.ByteString -> Id
 hashLazy MD5 = MD5_Id . MD5.hashlazy
+hashLazy SHA1 = SHA1_Id . SHA1.hashlazy
 hashLazy SHA3_256 = SHA3 len . (SHA3.hashlazy len) where len = sha3_256HashLength
 hashLazy CRC32 = CRC32_Id . L.toStrict . toLazyByteString . word32BE . crc32
 
 hash :: Algro -> B.ByteString -> Id
 hash MD5 = MD5_Id . MD5.hash
+hash SHA1 = SHA1_Id . SHA1.hash
 hash SHA3_256 = SHA3 len . (SHA3.hash len) where len = sha3_256HashLength
 hash CRC32 = CRC32_Id . L.toStrict . toLazyByteString . word32BE . crc32
 
@@ -67,6 +70,7 @@ createIdFromHex s =
 instance Show Id where
   show (CRC32_Id digest) = "crc32-" ++ (C.unpack $! Base16.encode digest)
   show (MD5_Id digest) = "md5-" ++ (C.unpack $! Base16.encode digest)
+  show (SHA1_Id digest) = "sha1-" ++ (C.unpack $! Base16.encode digest)
   show (SHA3 len digest) = "sha3_" ++ (show len) ++ "-" ++ (C.unpack $! Base16.encode digest)
   show (Nil) = "nil-"
 
@@ -79,18 +83,21 @@ instance Serialize Algro where
   put CRC32    = put (1 :: Word8)
   put MD5      = put (2 :: Word8)
   put SHA3_256 = put (3 :: Word8)
+  put SHA1     = put (4 :: Word8)
   get = do
     t <- get :: Get Word8
     case t of
       1 -> return CRC32
       2 -> return MD5
       3 -> return SHA3_256
+      4 -> return SHA1
       _ -> fail "Unrecognized"
 
 instance Serialize Id where
-  put (MD5_Id   h) = put MD5      >> put h
-  put (CRC32_Id h) = put CRC32    >> put h
-  put (SHA3 256 h) = put SHA3_256 >> put h
+  put (MD5_Id   d) = put MD5   >> put d
+  put (CRC32_Id d) = put CRC32 >> put d
+  put (SHA1_Id  d) = put SHA1  >> put d
+  put (SHA3 256 d) = put SHA3_256 >> put d
   put (SHA3 i _) = fail ("Unsuppored SHA3 size: " ++ (show i))
   put (Nil) = fail ("Cannot serialize nil conetent identifier")
 
@@ -99,6 +106,7 @@ instance Serialize Id where
     case a of
       CRC32 -> get >>= return . CRC32_Id
       MD5   -> get >>= return . MD5_Id
-      SHA3_256 -> get >>= return . SHA3 256
+      SHA1  -> get >>= return . SHA1_Id
+      SHA3_256 -> get >>= return . SHA3 sha3_256HashLength
 
 instance NFData Id where rnf = genericRnf
