8722b269cc86b31a9de617a4c93e87074a572610
Daniel Stiner <danstiner@gmail.com>
2015-01-03 23:23:41 -0800

    Remove unused imports

--- src/Command/ParseUtil.hs
+++ src/Command/ParseUtil.hs
@@ -1,32 +1,12 @@
 {-# LANGUAGE Rank2Types #-}
 
 module Command.ParseUtil (
-    pathspecSource
-  , PathSpec
+    PathSpec
 ) where
 
 import           Control.Monad
 import           Control.Monad.Trans (lift)
 import           Data.List           (delete)
-import           Data.Machine
 import           System.IO
 
 type PathSpec = FilePath
-
-pathspecSource :: [PathSpec] -> Bool -> SourceT IO PathSpec
-pathspecSource argPaths readMoreFromStdin =
-  if readMoreFromStdin || elem stdinFilename argPaths
-    then stdinAsLinesSource ~> prepended filteredArgPaths
-    else source filteredArgPaths
-  where
-    filteredArgPaths = delete stdinFilename argPaths
-    stdinFilename = "-"
-
-stdinAsLinesSource :: SourceT IO String
-stdinAsLinesSource = construct stdinLinesPlan where
-  stdinLinesPlan = do
-    eof <- lift isEOF
-    unless eof $ do
-      line <- lift getLine
-      yield line
-      stdinLinesPlan
