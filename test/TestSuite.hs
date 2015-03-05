52d76dd05d8f8913766873038161a2848b1d638b
Daniel Stiner <danstiner@gmail.com>
2015-03-04 23:54:40 -0800

    Fix build, stylish-haskell mangled HTF annotations on some imports

--- test/TestSuite.hs
+++ test/TestSuite.hs
@@ -1,10 +1,10 @@
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 module Main where
 
-import           Command.Remove
-import           Dedupe
-import           Repository
-import           Test.Framework
+import {-@ HTF_TESTS @-} Command.Remove
+import {-@ HTF_TESTS @-} Dedupe
+import {-@ HTF_TESTS @-} Repository
+import                   Test.Framework
 
 main :: IO ()
 main = htfMain htf_importedTests
