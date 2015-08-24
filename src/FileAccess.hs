697cbc5ac98650f54773fc061d4290c7d5f9f091
Daniel Stiner <danstiner@gmail.com>
2015-08-24 00:08:38 -0700

    Add create directory is missing functor to FileAccess monad

--- src/FileAccess.hs
+++ src/FileAccess.hs
@@ -12,6 +12,7 @@ import           System.FilePath       as FilePath
 data FileAccessF next
   = DoesDirectoryExist FilePath (Bool -> next)
   | ParentDirectory FilePath (FilePath -> next)
+  | CreateDirectoryIfMissing FilePath (next)
   deriving (Functor)
 
 type FileAccess = Free FileAccessF
@@ -21,9 +22,11 @@ makeFree ''FileAccessF
 runIO :: FileAccess a -> IO a
 runIO (Free (DoesDirectoryExist path f)) = Directory.doesDirectoryExist path >>= runIO . f
 runIO (Free (ParentDirectory path f)) = runIO . f $ FilePath.takeDirectory path
+runIO (Free (CreateDirectoryIfMissing path f)) = Directory.createDirectoryIfMissing False path >> runIO f 
 runIO (Pure a) = return a
 
 runPure :: [FilePath] -> FileAccess a -> a
 runPure paths (Free (DoesDirectoryExist path f)) = runPure paths . f $ path `elem` paths
 runPure paths (Free (ParentDirectory path f)) = runPure paths . f $ takeDirectory path
+runPure paths (Free (CreateDirectoryIfMissing path f)) = runPure (path : paths) f
 runPure _ (Pure a) = a
