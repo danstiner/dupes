8551af190455943e1ab9c10aa737bce72d018eca
Daniel Stiner <danstiner@gmail.com>
2015-12-27 23:47:48 -0800

    Database.SQLite case connection open and close

--- src/Database/SQLite.hs
+++ src/Database/SQLite.hs
@@ -1,13 +1,13 @@
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module Database (open, htf_thisModulesTests) where
+module Database.SQLite (open, integrationTests) where
 
 import           Control.Applicative
 import qualified Data.Text              as T
 import qualified Database.SQLite.Simple as SQLite
 
-import           Test.Framework
-import           Test.HUnit
+import           Test.Tasty.HUnit
+import           Test.Tasty.TH
 
 import           Repository             (Repository)
 
@@ -22,12 +22,14 @@ instance SQLite.FromRow TestField where
 open :: Repository -> IO DB
 open r = undefined
 
-test_openAndClose_Connection = do
+case_connection_open_and_close = do
   connection <- SQLite.open ":memory:"
   SQLite.execute_ connection
     (SQLite.Query $ T.pack "CREATE TABLE test (id INTEGER PRIMARY KEY, str text)")
   SQLite.execute connection (SQLite.Query $ T.pack "INSERT INTO test (str) VALUES (?)")
-    (SQLite.Only ("value"))
-  [(TestField id value)] <- SQLite.query_ connection (SQLite.Query $ T.pack "SELECT * FROM test") :: IO [TestField]
+    (SQLite.Only "value")
+  [TestField id value] <- SQLite.query_ connection (SQLite.Query $ T.pack "SELECT * FROM test") :: IO [TestField]
   "value" @=? value
   SQLite.close connection
+
+integrationTests = $(testGroupGenerator)
--- test/IntegrationTestSuite.hs
+++ test/IntegrationTestSuite.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import qualified Database.SQLite as SQLite
+
 import           Test.Tasty (defaultMain, testGroup)
 
 main :: IO ()
-main = defaultMain $ testGroup "Integration tests" []
+main = defaultMain $ testGroup "Integration tests" [SQLite.integrationTests]
