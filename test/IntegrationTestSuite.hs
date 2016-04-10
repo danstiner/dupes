1eda02bbcd1769e6a3958f40ec60cc80ee2ad696
Daniel Stiner <danstiner@gmail.com>
2016-04-09 23:44:05 -0700

    Formatting

--- test/IntegrationTestSuite.hs
+++ test/IntegrationTestSuite.hs
@@ -2,7 +2,7 @@ module Main where
 
 import qualified Database.SQLite as SQLite
 
-import           Test.Tasty (defaultMain, testGroup)
+import           Test.Tasty      (defaultMain, testGroup)
 
 main :: IO ()
 main = defaultMain $ testGroup "Integration tests" [SQLite.integrationTests]
