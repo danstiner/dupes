91889469f1e41047925b54e5cbcb5b15e12afc9e
Daniel Stiner <danstiner@gmail.com>
2016-04-09 23:23:13 -0700

    Implement keep which logic using a pipe

--- src/Keep.hs
+++ src/Keep.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Keep ( pureTests ) where
 
 import Control.Applicative
 import Control.Monad
 import Pipes
+import Pipes.Parse
 import qualified Pipes.Prelude as P
 import qualified Control.DeepSeq as DeepSeq
 
@@ -17,18 +19,62 @@ data Which = LeftOnly | RightOnly | Both deriving (Show)
 instance Arbitrary Which where
   arbitrary = elements [LeftOnly, RightOnly, Both]
 
-filterKeeping :: (Monad m) => (a -> a -> Which) -> Pipe a a m ()
-filterKeeping which = undefined
+parser :: (Monad m) => (a -> a -> Which) -> Parser a m (Maybe a)
+parser which = do
+  drawA <- draw
+  case drawA of
+    Nothing -> return Nothing
+    Just a -> do
+      drawB <- draw
+      case drawB of
+        Nothing -> return (Just a)
+        Just b -> case which a b of
+            LeftOnly  -> return (Just a)
+            RightOnly -> return (Just b)
+            Both      -> unDraw b *> return (Just a)
+
+{-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Right` result
+
+    Returns the remainder of the `Producer` when the `Parser` returns `Left`
+-}
+parsed
+    :: Monad m
+    => Parser a m (Either e b)
+    -> Producer a m r -> Producer b m (e, Producer a m r)
+parsed parser = go
+  where
+    go p = do
+        (x, p') <- lift (runStateT parser p)
+        case x of
+            Left  r -> return (r, p')
+            Right b -> do
+                yield b
+                go p'
+
+{-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Just` result
+
+    Sequences the remainder of the `Producer` when the `Parser` returns `Just`
+-}
+parsedForever_
+    :: Monad m
+    => Parser a m (Maybe a)
+    -> Producer a m ()
+    -> Producer a m ()
+parsedForever_ parser p = do
+    ((), p') <- parsed parser' p
+    p'
+  where
+    parser' = do
+        x <- parser
+        return (case x of
+            Nothing -> Left ()
+            Just b  -> Right b )
+
+filterKeeping :: (Monad m) => (a -> a -> Which) -> Producer a m () -> Producer a m ()
+filterKeeping which = parsedForever_ (parser which)
 
 filterListKeeping :: (a -> a -> Which) -> [a] -> [a]
-filterListKeeping which [] = []
-filterListKeeping which (a:b:xs) = case which a b of
-    LeftOnly  -> filterListKeeping which (a:xs)
-    RightOnly -> filterListKeeping which (b:xs)
-    Both      -> a : filterListKeeping which (b:xs)
-filterListKeeping which [a] = [a]
-
---P.toList (filterKeeping which <-< each as)
+filterListKeeping which xs = P.toList (filterKeeping which (each xs)) 
 
 prop_filter_keep_both_is_id :: [Int] -> Bool
 prop_filter_keep_both_is_id xs = filterListKeeping (const . const Both) xs == xs
