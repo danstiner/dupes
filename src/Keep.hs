0cd6c3df9678e65d0b4b15760c3969b687706a11
Daniel Stiner <danstiner@gmail.com>
2016-04-09 23:30:00 -0700

    Move pipes-parse extensions to appropriate file

--- src/Keep.hs
+++ src/Keep.hs
@@ -7,6 +7,7 @@ import Control.Applicative
 import Control.Monad
 import Pipes
 import Pipes.Parse
+import Pipes.Parse.Ext
 import qualified Pipes.Prelude as P
 import qualified Control.DeepSeq as DeepSeq
 
@@ -19,8 +20,8 @@ data Which = LeftOnly | RightOnly | Both deriving (Show)
 instance Arbitrary Which where
   arbitrary = elements [LeftOnly, RightOnly, Both]
 
-parser :: (Monad m) => (a -> a -> Which) -> Parser a m (Maybe a)
-parser which = do
+keepWhichParser :: (Monad m) => (a -> a -> Which) -> Parser a m (Maybe a)
+keepWhichParser which = do
   drawA <- draw
   case drawA of
     Nothing -> return Nothing
@@ -33,45 +34,8 @@ parser which = do
             RightOnly -> return (Just b)
             Both      -> unDraw b *> return (Just a)
 
-{-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Right` result
-
-    Returns the remainder of the `Producer` when the `Parser` returns `Left`
--}
-parsed
-    :: Monad m
-    => Parser a m (Either e b)
-    -> Producer a m r -> Producer b m (e, Producer a m r)
-parsed parser = go
-  where
-    go p = do
-        (x, p') <- lift (runStateT parser p)
-        case x of
-            Left  r -> return (r, p')
-            Right b -> do
-                yield b
-                go p'
-
-{-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Just` result
-
-    Sequences the remainder of the `Producer` when the `Parser` returns `Just`
--}
-parsedForever_
-    :: Monad m
-    => Parser a m (Maybe a)
-    -> Producer a m ()
-    -> Producer a m ()
-parsedForever_ parser p = do
-    ((), p') <- parsed parser' p
-    p'
-  where
-    parser' = do
-        x <- parser
-        return (case x of
-            Nothing -> Left ()
-            Just b  -> Right b )
-
 filterKeeping :: (Monad m) => (a -> a -> Which) -> Producer a m () -> Producer a m ()
-filterKeeping which = parsedForever_ (parser which)
+filterKeeping which = parsedForever_ (keepWhichParser which)
 
 filterListKeeping :: (a -> a -> Which) -> [a] -> [a]
 filterListKeeping which xs = P.toList (filterKeeping which (each xs)) 
---
+++ src/Pipes/Parse/Ext.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Pipes.Parse.Ext ( parsedForever_ ) where
+
+import Pipes
+import Pipes.Parse
+
+{-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Right` result
+
+    Returns the remainder of the `Producer` when the `Parser` returns `Left`
+-}
+parsed
+    :: Monad m
+    => Parser a m (Either e b)
+    -> Producer a m r -> Producer b m (e, Producer a m r)
+parsed parser = go
+  where
+    go p = do
+        (x, p') <- lift (runStateT parser p)
+        case x of
+            Left  r -> return (r, p')
+            Right b -> do
+                yield b
+                go p'
+
+{-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Just` result
+
+    Sequences the remainder of the `Producer` when the `Parser` returns `Just`
+-}
+parsedForever_
+    :: Monad m
+    => Parser a m (Maybe a)
+    -> Producer a m ()
+    -> Producer a m ()
+parsedForever_ parser p = do
+    ((), p') <- parsed parser' p
+    p'
+  where
+    parser' = do
+        x <- parser
+        return (case x of
+            Nothing -> Left ()
+            Just b  -> Right b )
