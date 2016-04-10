4b24629bce45238626d8acf362da61313754ea78
Daniel Stiner <danstiner@gmail.com>
2016-04-09 21:08:23 -0700

    Add property keeps at least one

--- src/Keep.hs
+++ src/Keep.hs
@@ -7,10 +7,14 @@ import Control.Monad
 import Pipes
 import qualified Pipes.Prelude as P
 
+import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck
 import           Test.Tasty.TH
 
 data Which = LeftOnly | RightOnly | Both
+
+instance Arbitrary Which where
+  arbitrary = elements [LeftOnly, RightOnly, Both]
 
 filterKeeping :: (Monad m) => (a -> a -> Which) -> Pipe a a m ()
 filterKeeping which = undefined
@@ -27,5 +31,13 @@ filterListKeeping which [a] = [a]
 
 prop_filter_keep_both_is_id :: [Int] -> Bool
 prop_filter_keep_both_is_id xs = filterListKeeping (const . const Both) xs == xs
+
+prop_filter_keeps_at_least_one :: NonEmptyList Int -> Property
+prop_filter_keeps_at_least_one (NonEmpty xs) = forAll (filterArbitrarily xs) (not . null)
+  where
+    filterArbitrarily :: [a] -> Gen [a]
+    filterArbitrarily xs = do
+      arbitraryWhich <- const . const <$> arbitrary
+      return $ filterListKeeping arbitraryWhich xs
 
 pureTests = $(testGroupGenerator)
