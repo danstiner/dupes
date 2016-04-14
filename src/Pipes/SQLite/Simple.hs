b79fadef03b731ae5fa7ddd8abd19fce791844e2
Daniel Stiner <danstiner@gmail.com>
2016-04-13 22:30:24 -0700

    Simplify test in Pipes.SQLite.Simple

--- src/Pipes/SQLite/Simple.hs
+++ src/Pipes/SQLite/Simple.hs
@@ -69,21 +69,12 @@ doQuery stmt =
           go
         Nothing -> return ()
 
-data TestField = TestField { i_ :: Int }
-  deriving Show
-
-instance FromRow TestField where
-  fromRow = TestField <$> SQLite.field
-
-instance ToRow TestField where
-  toRow (TestField i) = SQLite.toRow (SQLite.Only i)
-
 prop_query_produces_correct_rows_in_order :: [Int] -> Property
 prop_query_produces_correct_rows_in_order xs = monadicIO $ do
   xs' <- run $ SQLite.withConnection ":memory:" $ \connection -> do
            SQLite.execute_ connection "CREATE TABLE test (i INTEGER)"
-           mapM_ (SQLite.execute connection "INSERT INTO test (i) VALUES (?)" . TestField) xs
-           runSafeT $ P.toListM (query_ connection "SELECT i FROM test")
-  assert (xs == map i_ xs')
+           mapM_ (SQLite.execute connection "INSERT INTO test (i) VALUES (?)" . Only) xs
+           runSafeT $ P.toListM (query_ connection "SELECT i FROM test" >-> P.map fromOnly)
+  assert (xs == xs')
 
 integrationTests = $(testGroupGenerator)
