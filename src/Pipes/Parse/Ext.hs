8db89d4622ba411b8783fd004ef61841036f89a1
Daniel Stiner <danstiner@gmail.com>
2016-04-13 22:44:49 -0700

    Bump to stack lts-5.12
    Allows removing local copy of Pipes.Parse.parsed

--- src/Pipes/Parse/Ext.hs
+++ src/Pipes/Parse/Ext.hs
@@ -5,23 +5,6 @@ module Pipes.Parse.Ext (parsedForever_) where
 import           Pipes
 import           Pipes.Parse
 
-{-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Right` result
-
-    Returns the remainder of the `Producer` when the `Parser` returns `Left`
--}
-parsed :: Monad m
-       => Parser a m (Either e b)
-       -> Producer a m r -> Producer b m (e, Producer a m r)
-parsed parser = go
-  where
-    go p = do
-      (x, p') <- lift (runStateT parser p)
-      case x of
-        Left r -> return (r, p')
-        Right b -> do
-          yield b
-          go p'
-
 {-| Run a `Parser` repeatedly on a `Producer`, `yield`ing each `Just` result
 
     Sequences the remainder of the `Producer` when the `Parser` returns `Just`
