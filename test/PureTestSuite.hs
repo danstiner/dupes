d830047e73678df59099d18a5fdf5133230c7774
Daniel Stiner <danstiner@gmail.com>
2016-04-18 01:16:44 -0700

    Formatting

--- test/PureTestSuite.hs
+++ test/PureTestSuite.hs
@@ -1,17 +1,13 @@
 module Main where
 
-import qualified Dupes.Repository       as Repository
+import qualified Dupes.Repository as Repository
 import qualified Keep
 import qualified PathSpec
 import qualified Store
 
-import           Test.Tasty             (defaultMain, testGroup)
+import           Test.Tasty       (defaultMain, testGroup)
 
 main :: IO ()
 main = defaultMain $
   testGroup "Pure tests"
-    [ PathSpec.pureTestGroup
-    , Repository.pureTestGroup
-    , Keep.pureTests
-    , Store.pureTests
-    ]
+    [PathSpec.pureTestGroup, Repository.pureTestGroup, Keep.pureTests, Store.pureTests]
