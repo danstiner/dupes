533b7270e2140149fad329bb7ae2d0d95360cbfc
Daniel Stiner <danstiner@gmail.com>
2016-04-19 00:03:03 -0700

    Walk WorkingDirectory in Actions.update

--- src/Dupes/Actions.hs
+++ src/Dupes/Actions.hs
@@ -1,10 +1,14 @@
 module Dupes.Actions (update) where
 
 import           Dupes.Repository
+import           Dupes.WorkingDirectory
 import           Pipes
+import qualified Pipes.Path             as Path
+import qualified Pipes.Prelude          as P
 import           Pipes.Safe
 
-data UpdatedIndexEntry = UpdatedIndexEntry
+data UpdatedIndexEntry = UpdatedIndexEntry FilePath
+  deriving Show
 
 update :: Repository -> Producer UpdatedIndexEntry (SafeT IO) ()
-update = undefined
+update repository = walk (workingDirectory repository) >-> P.map (UpdatedIndexEntry . Path.getPath)
